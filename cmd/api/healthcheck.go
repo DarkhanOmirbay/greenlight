fcf946e5e47e37153cbe24b25720d81f9aab0b15
DarkhanOmirbay <domirbai04@gmail.com>
2024-03-29 23:23:40 +0500

    encoding go obj to json,writeJSON helper

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -11,10 +10,31 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	//fmt.Fprintln(w, "status: available")
 	//fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	//fmt.Fprintf(w, "version: %s\n", version)
-	js := `{"status":"available", "enviroment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
 
-	w.Header().Set("Content-Type", "application/json")
+	//js := `{"status":"available", "enviroment": %q, "version": %q}`
+	//js = fmt.Sprintf(js, app.config.env, version)
+	//
+	//w.Header().Set("Content-Type", "application/json")
+	//
+	//w.Write([]byte(js))
 
-	w.Write([]byte(js))
+	data := map[string]string{
+		"status":     "available",
+		"enviroment": app.config.env,
+		"version":    version,
+	}
+	//js, err := json.Marshal(data)
+	//if err != nil {
+	//	app.logger.Print(err)
+	//	http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	//	return
+	//}
+	//js = append(js, '\n')
+	//w.Header().Set("Content-Type", "application/json")
+	//w.Write([]byte(js))
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
